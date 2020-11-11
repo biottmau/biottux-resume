5bbe0d1cf2fe23ae13ab1a053940a317619b476b
Mauricio Biott <mauro@biott.com.ar>
2020-11-10 23:13:34 -0300

    titulo animado

--- components/sections/Land.js
+++ components/sections/Land.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './sections.module.css'
 
 const Land = () => {
+    const [title_class, setTitleClass] = useState(styles.section_land_title_off);
+
+    // Animation for title
+    useEffect(() => {
+        const timer = setTimeout( () => {
+            setTitleClass(styles.section_land_title)
+        } ,300);
+        return () => clearInterval(timer);
+    }),[];
 
     return (
         <div className={styles.section_land}>
-            Hola
+            <div className={title_class}>
+                @Biottux 
+                <div className={styles.section_land_subtitle}>| Software Development</div>
+            </div>
         </div>
     )
 }
